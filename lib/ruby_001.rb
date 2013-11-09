3e08f3ecbd3a79609a5557bfae989bde9eea2779
alpaca-tc <alprhcp666@gmail.com>
2013-11-09 22:24:59 +0900

    Append description about question to ruby_001

--- lib/ruby_001.rb
+++ lib/ruby_001.rb
@@ -1,3 +1,5 @@
+# Q1. [エイトクイーン問題](http://ja.wikipedia.org/wiki/%E3%82%A8%E3%82%A4%E3%83%88%E3%83%BB%E3%82%AF%E3%82%A4%E3%83%BC%E3%83%B3)
+# 与えられたマスのサイズに対して、バリエーション解の数を答えてください
 class Solver
   def initialize(size)
     @size = size
